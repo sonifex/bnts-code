b570c546653df0fcb30df1acc9845e311d17bd0f
Soner Guler <sonerguler93@gmail.com>
2023-08-10 11:11:09 +0100

    Initial Commit

---
+++ bnts-code/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bnts-code
+//
+//  Created by Soner Güler on 10/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ bnts-codeUITests/bnts_codeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  bnts_codeUITestsLaunchTests.swift
+//  bnts-codeUITests
+//
+//  Created by Soner Güler on 10/08/2023.
+//
+
+import XCTest
+
+final class bnts_codeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
